28865459bd8e0f5ef68ff9fba3ee6bf2a2c6d27c
Abdelrahman2656 <a.elmonged870@gmail.com>
2024-12-20 20:59:15 +0200

    updated success

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -34,7 +34,7 @@ export const signUp = async (req, res, next) => {
 
       await saveVerificationData({ email, verificationCode, expirationTime });
 
-      const verificationLink = `${req.protocol}://${req.headers.host}api/v1/verify/${token}`;
+      const verificationLink = `${req.protocol}://${req.headers.host}/api/v1/verify/${token}`;
       const emailContent = emailHtml(
         verificationLink,
         verificationCode,
@@ -89,7 +89,7 @@ export const signUp = async (req, res, next) => {
   await saveVerificationData({ email, verificationCode, expirationTime });
 
   // Create the verification link and email content
-  const verificationLink = `${req.protocol}://${req.headers.host}api/v1/verify/${token}`;
+  const verificationLink = `${req.protocol}://${req.headers.host}/api/v1/verify/${token}`;
   const emailContent = emailHtml(
     verificationLink,
     verificationCode,
@@ -225,7 +225,7 @@ export const resendVerificationCode = async (req, res, next) => {
       });
 
       // Create the verification link and email content
-      const verificationLink = `${req.protocol}://${req.headers.host}api/v1/verify/${token}`;
+      const verificationLink = `${req.protocol}://${req.headers.host}/api/v1/verify/${token}`;
       const emailContent = emailHtml(
         verificationLink,
         newVerificationCode,
