8cf5ea72599d01ab09b57c1254d071babb7cc7f9
Abdelrahman2656 <142105484+Abdelrahman2656@users.noreply.github.com>
2024-12-20 19:30:40 +0200

    Update dbconnection.js

--- Database/dbconnection.js
+++ Database/dbconnection.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 export const dbconnection = () => {
-    return mongoose.connect(process.env.DB_URL,{ useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+    return mongoose.connect(process.env.DB_URL).then(() => {
         console.log(`db connected successfully ${process.env.DB_URL}`);
     }).catch((err) => {
         console.log('field to connect to db');
